6d0b8ff39a2f23e4e786357c45c3b2fcc017a984
K1Mi <62942171+K1Mi1338@users.noreply.github.com>
2024-02-13 00:04:24 +1000

    1.0

---
+++ gayornatural.py
@@ -0,0 +1,8 @@
+pidor = input("Вы гей? ")
+if pidor == "Да" or pidor == "ДА" or pidor == "да":
+    print("Поздравляем, вы ТОЧНО гей")
+elif pidor == "Нет" or pidor == "НЕТ" or pidor == "нет":
+    print("Вы не гей :(")
+else:
+    print("Что угодно")
+input()
